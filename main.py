dfbf640cb53f68a39d827ee85c6d7a6ea1608082
Joseph Bokan <jjbokan3@gmail.com>
2025-12-30 14:35:07 -0500

    Add new Prefect task and integrate into existing flow

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from prefect import flow, task
 def text():
     print("WHAT UP")
 
+@task(log_prints=True)
+def adding():
+    print(10+10)
+    return 100
+
 @flow(log_prints=True)
 def my_flow():
     text()
+    adding()
     print("Hello, world!")
 
 
