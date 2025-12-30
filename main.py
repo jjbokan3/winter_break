8d8d19bd12a92f8290b39e7f50d5300917c4e6bd
Joseph Bokan <jjbokan3@gmail.com>
2025-12-30 15:09:53 -0500

    Update `my_flow` with timeout, error raising, and webhook integration

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def adding():
     print(10+10)
     return 100
 
-@flow()
+@flow(timeout_seconds=600)
 def my_flow():
     logger = get_run_logger()
     logger.info("Hello, world!")
@@ -19,8 +19,11 @@ def my_flow():
     logger.info("Hello again!")
     text()
     adding()
+    raise Exception("This is an error!")
     print("Hello, world!")
 
 
 if __name__ == "__main__":
     my_flow()
+
+https://discord.com/api/webhooks/1455653451487772745/YJsGOa3BwRKPHwrHtmv1bTJ7HbTzBz9Db9HUsDm29k1Qvyya8xZ_Hlc4Tc1K-e6rxToh
