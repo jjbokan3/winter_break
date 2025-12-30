c72aa30c882df3c8a56456a2aac09bf3d6458b20
Joseph Bokan <jjbokan3@gmail.com>
2025-12-30 15:14:46 -0500

    Add Exception raising to `my_flow` and remove webhook URL

--- main.py
+++ main.py
@@ -19,10 +19,9 @@ def my_flow():
     logger.info("Hello again!")
     text()
     adding()
+    raise Exception("This is an error!")
     print("Hello, world!")
 
 
 if __name__ == "__main__":
     my_flow()
-
-https://discord.com/api/webhooks/1455653451487772745/YJsGOa3BwRKPHwrHtmv1bTJ7HbTzBz9Db9HUsDm29k1Qvyya8xZ_Hlc4Tc1K-e6rxToh
